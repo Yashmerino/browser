bca1ac3b3090dd9bf5db7397e1f016226a00017a
Artiom Bozieac <artiomytprofile@gmail.com>
2025-05-08 18:49:01 +0300

    Add basic version of browser

--- browser/BrowserForm.cs
+++ browser/BrowserForm.cs
@@ -2,9 +2,40 @@ namespace browser
 {
     public partial class BrowserForm : Form
     {
+        
+
         public BrowserForm()
         {
             InitializeComponent();
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            WebBrowser.Reload();
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            WebBrowser.GoBack();
+        }
+
+        private void ForwardButton_Click(object sender, EventArgs e)
+        {
+            WebBrowser.GoForward();
+        }
+
+        private void UrlField_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try { 
+                    WebBrowser.Source = new UriBuilder(urlField.Text).Uri;
+                    WebBrowser.Focus();
+                } catch(UriFormatException ex) {
+                    Console.WriteLine("Malformed URL: " + ex);
+                    return;
+                }
+            }
         }
     }
 }
--- browser/browserForm.Designer.cs
+++ browser/browserForm.Designer.cs
@@ -17,12 +17,123 @@
 
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Simple Browser";
+            components = new System.ComponentModel.Container();
+            notifyIcon1 = new NotifyIcon(components);
+            panel1 = new Panel();
+            panel2 = new Panel();
+            RefreshButton = new Button();
+            ForwardButton = new Button();
+            BackButton = new Button();
+            urlField = new TextBox();
+            WebBrowser = new Microsoft.Web.WebView2.WinForms.WebView2();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)WebBrowser).BeginInit();
+            SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 26);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(RefreshButton);
+            panel2.Controls.Add(ForwardButton);
+            panel2.Controls.Add(BackButton);
+            panel2.Controls.Add(urlField);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 26);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 24);
+            panel2.TabIndex = 1;
+            // 
+            // RefreshButton
+            // 
+            RefreshButton.Dock = DockStyle.Left;
+            RefreshButton.Location = new Point(56, 0);
+            RefreshButton.Name = "RefreshButton";
+            RefreshButton.Size = new Size(28, 24);
+            RefreshButton.TabIndex = 3;
+            RefreshButton.Text = "button3";
+            RefreshButton.UseVisualStyleBackColor = true;
+            RefreshButton.Click += RefreshButton_Click;
+            // 
+            // ForwardButton
+            // 
+            ForwardButton.Dock = DockStyle.Left;
+            ForwardButton.Location = new Point(28, 0);
+            ForwardButton.Name = "ForwardButton";
+            ForwardButton.Size = new Size(28, 24);
+            ForwardButton.TabIndex = 2;
+            ForwardButton.Text = "button2";
+            ForwardButton.UseVisualStyleBackColor = true;
+            ForwardButton.Click += ForwardButton_Click;
+            // 
+            // BackButton
+            // 
+            BackButton.Dock = DockStyle.Left;
+            BackButton.Location = new Point(0, 0);
+            BackButton.Name = "BackButton";
+            BackButton.Size = new Size(28, 24);
+            BackButton.TabIndex = 1;
+            BackButton.Text = "button1";
+            BackButton.UseVisualStyleBackColor = true;
+            BackButton.Click += BackButton_Click;
+            // 
+            // urlField
+            // 
+            urlField.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            urlField.Location = new Point(90, 0);
+            urlField.Name = "urlField";
+            urlField.Size = new Size(710, 23);
+            urlField.TabIndex = 0;
+            urlField.KeyDown += UrlField_KeyDown;
+            // 
+            // WebBrowser
+            // 
+            WebBrowser.AllowExternalDrop = true;
+            WebBrowser.CreationProperties = null;
+            WebBrowser.DefaultBackgroundColor = Color.White;
+            WebBrowser.Dock = DockStyle.Fill;
+            WebBrowser.Location = new Point(0, 50);
+            WebBrowser.Name = "WebBrowser";
+            WebBrowser.Size = new Size(800, 400);
+            WebBrowser.Source = new Uri("https://google.com", UriKind.Absolute);
+            WebBrowser.TabIndex = 2;
+            WebBrowser.ZoomFactor = 1D;
+            // 
+            // BrowserForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(WebBrowser);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "BrowserForm";
+            Text = "Simple Browser";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)WebBrowser).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+        private NotifyIcon notifyIcon1;
+        private Panel panel1;
+        private Panel panel2;
+        private Microsoft.Web.WebView2.WinForms.WebView2 WebBrowser;
+        private TextBox urlField;
+        private Button RefreshButton;
+        private Button ForwardButton;
+        private Button BackButton;
     }
 }
